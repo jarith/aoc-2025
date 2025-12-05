8e15e81b86450ee5d4c7cee11b7bb36d73ae74b8
jarith <gorodilov.box@gmail.com>
2025-12-05 03:16:19 +0100

    day 3 solution

--- src/day3/part1.clj
+++ src/day3/part1.clj
@@ -1,1 +1,26 @@
-(ns day3.part1)
+(ns day3.part1
+  (:require [clojure.java.io :as io]))
+
+(defn read-instructions [path]
+  (with-open [reader (io/reader path)]
+    (into [] (line-seq reader))))
+
+(defn max-joltage
+  ([batteries]
+   (max-joltage 0 batteries))
+
+  ([max-joltage block]
+   (if-let [[battery1 & tail] (seq block)]
+     (if-let [tail' (seq tail)]
+       (let [battery2 (apply max-key int tail')
+             joltage  (Integer/parseInt (str battery1 battery2))]
+         (recur (max max-joltage joltage) tail))
+       max-joltage)
+     max-joltage)))
+
+(defn run []
+  (->> (read-instructions "src/day3/input.txt")
+       (map max-joltage)
+       (reduce + 0)))
+
+(run)
--- src/day3/part2.clj
+++ src/day3/part2.clj
@@ -1,1 +1,36 @@
-(ns day3.part2)
+(ns day3.part2
+  (:require [clojure.java.io :as io]))
+
+(defn read-instructions [path]
+  (with-open [reader (io/reader path)]
+    (into [] (line-seq reader))))
+
+(defn max-digit [block]
+  (->> block
+       (map-indexed vector)
+       (reduce (fn [acc pair]
+                 (if (> (long (second pair)) (long (second acc)))
+                   pair
+                   acc)))))
+
+(defn max-joltage [n batteries]
+  (let [len (count batteries)]
+    (loop [i   0
+           pos 0
+           acc ""]
+      (if (= i n)
+        acc
+        (let [remaining (- n i)
+              end (- len remaining)
+              block (subs batteries pos (inc end))
+              [idx battery] (max-digit block)
+              new-pos (inc (+ pos idx))]
+          (recur (inc i) new-pos (str acc battery)))))))
+
+(defn run []
+  (->> (read-instructions "src/day3/input.txt")
+       (map #(max-joltage 12 %))
+       (map #(Long/parseLong %))
+       (reduce + 0)))
+
+(run)
